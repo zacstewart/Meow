69072f7717ae93bfb0abad30989e80f27b0e10fb
Zac Stewart <zgstewart@gmail.com>
2011-05-15 02:11:14 -0400

    stored each setTimeout instance within the meow it destroys

--- jquery.meow.js
+++ jquery.meow.js
@@ -3,22 +3,22 @@
   var current_id = 0;
 
   function Meow() {
+    var that = this;
     this.id = 0;
     this.message = '';
     this.timestamp = Date.now();
     this.duration = 2400;
 
     this.draw = function() {
-      that = this;
-      console.log(this.message);
-      $('#meows').append($('<div id="meow-' + this.id + '">' + this.message + '</div>').hide().fadeIn(400));
-      setTimeout('that.destroy()', this.duration);
+      console.log(that.message);
+      $('#meows').append($('<div id="meow-' + that.id + '" class="meow">' + that.message + '</div>').hide().fadeIn(400));
+      that.timeout = setTimeout(function() { that.destroy(); }, that.duration);
     };
 
     this.destroy = function( ) {
       console.log('removing');
-      $('#meow-' + that.id).fadeOut(400, function() { $(this).remove() });
-      that.delete;
+      console.log(that);
+      $('#meow-' + that.id).fadeOut(400, function() { $(that).remove(); });
     };
   }
 
@@ -31,7 +31,7 @@
       meows.push(meow);
       current_id++;
     }
-  }
+  };
 
   $.fn.meow = function( event, message ) {
     return this.each(function() {
@@ -44,6 +44,5 @@
         }
       }
     });
-
   };
-})( jQuery );
+}( jQuery ));
