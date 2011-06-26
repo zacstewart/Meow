97404713601c4c8cdc2e19cf571574d0acc9ae69
Zac Stewart <zgstewart@gmail.com>
2011-06-26 15:48:03 -0400

    added more options, but now message is not updating after you init Meow

--- jquery.meow.js
+++ jquery.meow.js
@@ -4,34 +4,39 @@
   var meows = {},
     methods = {};
 
-  function Meow(message, icon) {
+  function Meow(options) {
     var that = this;
-    this.message = message;
-    this.icon = icon;
+    this.title = options.title
+    this.message = options.message;
+    this.icon = options.icon;
     this.timestamp = Date.now();
     this.duration = 2400;
     this.hovered = false;
-    this.dead = false;
     this.manifest = {};
     $('#meows').append($(document.createElement('div'))
-      .attr('id', 'meow-' + that.timestamp)
+      .attr('id', 'meow-' + this.timestamp)
       .addClass('meow')
-      .html($(document.createElement('div')).addClass('inner').text(that.message))
+      .html($(document.createElement('div')).addClass('inner').text(this.message))
       .hide()
       .fadeIn(400));
 
+    this.manifest = $('#meow-' + this.timestamp);
 
-    that.manifest = $('#meow-' + that.timestamp);
+    if (typeof this.title === 'string') {
+      this.manifest.find('.inner').prepend(
+        $(document.createElement('h1')).text(this.title)
+      );
+    }
 
     if (typeof that.icon === 'string') {
-      that.manifest.find('.inner').prepend(
+      this.manifest.find('.inner').prepend(
         $(document.createElement('div')).addClass('icon').html(
-          $(document.createElement('img')).attr('src', icon)
+          $(document.createElement('img')).attr('src', this.icon)
         )
       );
     }
 
-    that.manifest.bind('mouseenter mouseleave', function (event) {
+    this.manifest.bind('mouseenter mouseleave', function (event) {
       if (event.type === 'mouseleave') {
         that.hovered = false;
         that.manifest.removeClass('hover');
@@ -44,8 +49,8 @@
       }
     });
 
-    that.timeout = setTimeout(function () {
-      if (that.hovered !== true && typeof that === 'object' && that.dead !== true) {
+    this.timeout = setTimeout(function () {
+      if (that.hovered !== true && typeof that === 'object') {
         that.destroy();
       }
     }, that.duration);
@@ -54,7 +59,6 @@
       that.manifest.find('.inner').fadeTo(400, 0, function () {
         that.manifest.slideUp(function () {
           that.manifest.remove();
-          that.dead = true;
           delete meows[that.timestamp];
         });
       });
@@ -62,44 +66,78 @@
   }
 
   methods = {
-    createMessage: function (message, icon) {
-      var meow = new Meow(message, icon);
+    configMessage: function (options) {
+      var trigger,
+        title,
+        message,
+        icon,
+        message_type;
+
+
+      if (typeof options.title === 'string') {
+        title = options.title;
+      }
+
+      if (typeof options.message === 'string') {
+        message_type = 'string';
+      } else if (typeof options.message === 'object') {
+        message_type = options.message.first().nodeName;
+        // console.log(options.message.attr('title'));
+        if (typeof title === 'undefined' && typeof options.message.attr('title') === 'string') {
+          title = options.message.attr('title');
+        }
+      }
+
+      switch (message_type) {
+      case 'string':
+        message = options.message;
+        break;
+      case 'INPUT':
+      case 'SELECT':
+      case 'TEXTAREA':
+        message = options.message.attr('value');
+        break;
+      default:
+        message = options.message.text();
+        break;
+      }
+
+      if (typeof options.icon === 'string') {
+        icon = options.icon;
+      }
+      return {
+        trigger: trigger,
+        message: message,
+        icon: icon,
+        message_type: message_type
+      }
+    },
+    createMessage: function (options) {
+      var meow = new Meow(options);
       meows[meow.timestampe] = meow;
     }
   };
 
   $.fn.meow = function (options) {
-    var trigger,
-      message,
-      icon;
+    var options,
+      trigger;
     return this.each(function () {
       if (typeof options === 'string') {
-        event = options;
-      } else if (typeof options == 'object') {
+        trigger = options;
+      } else if (typeof options === 'object') {
         // set the event
         if (typeof options.trigger === 'string') {
           trigger = options.trigger;
         }
-        // is the message an object we need to parse or just a string?
-
       }
       if (typeof trigger === 'string') {
         $(this).bind(trigger, function () {
-          if (typeof options.message === 'string') {
-            message = options.message;
-          } else if (typeof options.message === 'object') {
-            var type = options.message[0].nodeName;
-            if ($.inArray(type, ['INPUT', 'SELECT', 'TEXTAREA']) !== -1) {
-              message = options.message.attr('value');
-            } else {
-              message = options.message.text();
-            }
-          }
-          if (typeof options.icon === 'string') {
-            icon = options.icon;
-          }
-          methods.createMessage(message, icon);
+          options = methods.configMessage(options);
+          methods.createMessage(options);
         });
+      } else if (typeof trigger === 'undefined') {
+        options = methods.configMessage(options);
+        methods.createMessage(options);
       }
     });
   };
